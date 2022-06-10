9e8c33dbdac6080a0e1b774a2231f011a345e0f4
Nijumdip <nijumdip019911@gmail.com>
2022-06-10 20:47:32 +0600

    added 1st section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CTA from './CTA';
 import './Header.css'
 
 const Header = () => {
@@ -6,6 +7,9 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
+        <h1>Shamima Akter</h1>
+        <h5 className='text-light'>Front-End Developer</h5>
+        <CTA></CTA>
       </div>
     </header>
   )
