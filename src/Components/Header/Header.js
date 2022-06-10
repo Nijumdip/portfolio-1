2b2bb5c1bcb2a599a8f8bd6825e09fd58645e273
Nijumdip <nijumdip019911@gmail.com>
2022-06-10 19:44:14 +0600

    added header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,11 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+      </div>
+    </header>
   )
 }
 
