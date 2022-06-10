01f38977b1b4290fb5bcd127323334b9011e448a
Nijumdip <nijumdip019911@gmail.com>
2022-06-10 22:28:10 +0600

    added icons

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Portfolio from './Components/Portfolio/Portfolio';
 import Testimonials from './Components/Testimonials/Testimonials';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <>
-      App
       <Header></Header>
       <Nav></Nav>
       <About></About>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import CTA from './CTA';
-import './Header.css';
-import ME from '../../assets/nijum_pic-removebg-preview.png'
-import HeaderSocials from './HeaderSocials';
+import React from "react";
+import CTA from "./CTA";
+import "./Header.css";
+import ME from "../../assets/nijum_pic-removebg-preview.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Shamima Akter</h1>
-        <h5 className='text-light'>Front-End Developer</h5>
-        <CTA/>
-        <HeaderSocials/>
+        <div className="gap">
+          <h5>Hello I'm</h5>
+          <h1>Shamima Akter</h1>
+          <h5 className="text-light">Front-End Developer</h5>
+          <CTA />
+        </div>
 
-        <div className='me'>
+        <div>
+          <HeaderSocials />
+        </div>
+
+        <div>
           <img src={ME} alt="" />
         </div>
-        <a href="#contact" className="scroll__down" >Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
