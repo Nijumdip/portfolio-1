66dd1594b9da7bdda62695bf86095a77b3e9cb8e
Nijumdip <nijumdip019911@gmail.com>
2022-06-12 08:21:00 +0600

    all done alhamdulillah

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,7 +8,7 @@ import { BiUserCircle } from "react-icons/bi";
 const About = () => {
   return (
     <section id="about">
-      <h5 className="pb-5">About Me</h5>
+      <h2 className="pb-5">About Me</h2>
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me-image">
